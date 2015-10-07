279a600c747f0f009731db7a6e01ade05f8976b3
peterfalconer <p.falconer@acfonline.org.au>
2015-10-07 20:30:14 +1100

    Added function to more click handler

--- code/js/script.js
+++ code/js/script.js
@@ -5,11 +5,14 @@ $(document).ready(function() {
                     ["30 Oct 2015","Poetry Reading","Cafe Climate","Hilary Bentley", "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. <b>Lorem</b> Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum <b>Sed</b> imperdiet. Duis sagittis ipsum. <b>Nulla</b> Praesent mauris. Fusce <b>sem</b> nec tellus <b>sagittis</b> sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti <b>lacinia</b> sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in <b>conubia</b> libero. Sed dignissim <b>nostra,</b> lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem <b>lacinia</b> at dolor. Maecenas <b>Pellentesque</b> mattis. Sed convallis tristique <i>libero.</i> sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis <b>vel</b> vel, suscipit quis, <b>egestas</b> luctu.</p>"],
                     ["23 Nov 2015","Public Meeting","Melbourne Town Hall","ACF", "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. <b>Lorem</b> Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum <b>Sed</b> imperdiet. Duis sagittis ipsum. <b>Nulla</b> Praesent mauris. Fusce <b>sem</b> nec tellus <b>sagittis</b> sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti <b>lacinia</b> sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in <b>conubia</b> libero. Sed dignissim <b>nostra,</b> lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem <b>lacinia</b> at dolor. Maecenas <b>Pellentesque</b> mattis. Sed convallis tristique <i>libero.</i> sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis <b>vel</b> vel, suscipit quis, <b>egestas</b> luctu.</p>"]]
 
-    // $(".showEvent").click(jumpToShowEvent.bind(null, e));
+    $(".showEvent").click(function(e) {
+        e.preventDefault();
+
+        window.location = "create.html";
+    });
 
     $(".extraInfo").click(function(e) {
-        console.log(e);
-        showExtraInfo.bind(this, e);
+        console.log(e);        
     });
     
     function showEventList(eventList) {
@@ -75,7 +78,7 @@ $(document).ready(function() {
                 "<li class=\"horizontal-list\"><button id=\"more" + i + "\">more</button></li>" +
                 "</ul><div class= \"extraInfo\"><div id=\"extraInfo" + i + "\">" + w[4] + "</div></div>";
 
-         return listItems;   
+         return listItems;
     }
 
     function addMoreClickHandler(NumEvents) {
@@ -83,8 +86,13 @@ $(document).ready(function() {
             console.log("Adding more" + j  + " click function");
 
             $("#more" + j).click(function(e){
-                console.log("more");
-                // $(e.currentTarget.parentNode).after();
+                console.log($(e.currentTarget.parentNode.parentNode.nextSibling));
+                $(e.currentTarget.parentNode.parentNode.nextSibling).toggleClass("extraInfo");
+                if (e.currentTarget.innerText === "more") {
+                    $(e.currentTarget).html("less");
+                } else {
+                    $(e.currentTarget).html("more");
+                }                
             });
         }
     }
